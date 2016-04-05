63eb7953567dbc706904774b0bd75ecdff3b2d75
Eric <ericdelsams@gmail.com>
2016-04-05 16:58:09 +0200

    final clase

--- ambr.py
+++ ambr.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from commandlist import CommandList
+import channels as ch
+import time
 class Ambrosio(object):
 	"""Class for Ambrosio Personal Digital Butler
 
 	will run our house"""
 	def __init__(self):
 		super(Ambrosio,self).__init__()
+		self.cl = CommandList()
+		self.channels = []
+		self.channels.append(ch.TextChannel())
 
 	def next_command(self):
 		try:
 			return self.cl.next()
 		except:
 			return None
-	
+	def update_channels(self):
+		for chan in self.channels:
+			while chan.msg_avail():
+				self.cl.append(chan.get_msg())
+
 	def mainloop(self):
 		#while True:
 		#	command = get_command
@@ -21,7 +30,12 @@ class Ambrosio(object):
 		#	update
 		while True:
 			command = self.next_command()
+			if command:
+				print command
 			time.sleep(1)
+			self.update_channels()
 
 if __name__ == "__main__":
 	print "here be dragons"
+	amb = Ambrosio()
+	amb.mainloop()
