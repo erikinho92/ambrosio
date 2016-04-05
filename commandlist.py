ed1f0c5d0a25456de99a173d7f58faf43a517279
Eric <ericdelsams@gmail.com>
2016-04-05 17:07:25 +0200

    final clase

--- commandlist.py
+++ commandlist.py
@@ -2,11 +2,10 @@
 
 
   class CommandList(object):
+
       def __init__(self):
           super(CommandList, self).__init__()
-          self.commands =[]
-
-
+          self.commands = []
 
       def next(self):
           return self.commands.pop(0)
