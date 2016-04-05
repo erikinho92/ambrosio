9d41953c3198a2bb73ecc15b7158e34eeb77df6d
Eric <ericdelsams@gmail.com>
2016-04-05 16:08:09 +0200

    llista comando

---
+++ commandlist.py
@@ -0,0 +1,16 @@
+
+
+
+  class CommandList(object):
+    def __init__(self):
+      super(CommandList, self).__init__()
+      self.commands =[]
+
+    def next(self):
+      pass
+
+    def append(self, command):
+      pass
+
+    def length(self):
+      pass
