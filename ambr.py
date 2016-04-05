cb75d2a52ab954965a9ed2ff220f16bf3df59c6a
Eric <ericdelsams@gmail.com>
2016-04-05 16:33:24 +0200

    anar fent

--- ambr.py
+++ ambr.py
@@ -8,13 +8,20 @@ class Ambrosio(object):
 	def __init__(self):
 		super(Ambrosio,self).__init__()
 
+	def next_command(self):
+		try:
+			return self.cl.next()
+		except:
+			return None
+	
 	def mainloop(self):
 		#while True:
 		#	command = get_command
 		# 	do_command(command)
 		#	update
-		pass
-
+		while True:
+			command = self.next_command()
+			time.sleep(1)
 
 if __name__ == "__main__":
 	print "here be dragons"
