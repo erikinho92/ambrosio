6abe0ebe25ecfece51578ae34813253e0906d3b5
Eric <ericdelsams@gmail.com>
2016-04-05 17:01:35 +0200

    final clase1

--- channels.py
+++ channels.py
@@ -9,7 +9,8 @@ class ClassName(object):
 class TextChannel(object):
     """Channel class, reads commands from file"""
     def __init__(self, name="TextChannel"):
-        super(TextChannel, self).__init__(name)
+        super(TextChannel, self).__init__()
+        self.name = name 
         self.messages = []
         with open("messages.txt", "r") as f:
             for line in f:
