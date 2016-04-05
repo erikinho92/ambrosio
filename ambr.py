1da7cf4c54d4192688c9c915b8508fc138ef8120
Eric <ericdelsams@gmail.com>
2016-04-05 16:02:15 +0200

    primer commit

---
+++ ambr.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Ambrosio(object):
+	"""Class for Ambrosio Personal Digital Butler
+
+	will run our house"""
+	def __init__(self):
+		super(Ambrosio,self).__init__()
+
+	def mainloop(self):
+		#while True:
+		#	command = get_command
+		# 	do_command(command)
+		#	update
+		pass
+
+
+if __name__ == "__main__":
+	print "here be dragons"
